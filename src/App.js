3b14b7e7d4399c39deccb8cdc468740395b2a0b0
Sharon0207 <osemwengiesharon0207@gmail.com>
2024-10-08 09:46:39 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Banner from "./components/home/Banner";
+
+function App() {
+  return (
+    <div className="font-bodyFont">
+      <Header/>
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Slider from "react-slick";
+
+const Banner = () => {
+  const  settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      };
+  return (
+    <div>Banner</div>
+  )
+}
+
+export default Banner
