f42afa097fcf2b3cefee531f95b809696f8839d7
Sharon0207 <osemwengiesharon0207@gmail.com>
2024-10-09 09:51:01 +0100

    Initial commit

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -10,7 +10,28 @@ const Banner = () => {
         slidesToScroll: 1,
       };
   return (
-    <div>Banner</div>
+    <div>
+    <Slider {...settings}>
+    <div>
+      <h3>1</h3>
+    </div>
+    <div>
+      <h3>2</h3>
+    </div>
+    <div>
+      <h3>3</h3>
+    </div>
+    <div>
+      <h3>4</h3>
+    </div>
+    <div>
+      <h3>5</h3>
+    </div>
+    <div>
+      <h3>6</h3>
+    </div>
+  </Slider>
+  </div>
   )
 }
 
